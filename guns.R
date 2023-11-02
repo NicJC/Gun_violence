61b5b303d3e660387c4602df09d01461f7777e17
“[Nic <[nicjcoxen@gmail.com]>
2023-11-02 22:40:43 +1300

    png

--- guns.R
+++ guns.R
@@ -4,10 +4,11 @@ knitr::opts_chunk$set(echo = FALSE)
 knitr::opts_chunk$set(message = FALSE)
 opts_chunk$set(comment="", fig.align="center", tidy=TRUE , cache=TRUE)
 library(tidyverse)
+library(data.table)
 
 
 
-guns <- read_csv("C:/R/Mass_Shootings.csv")
+guns <- fread("C:/R/Mass_Shootings.csv")
 
 
 
